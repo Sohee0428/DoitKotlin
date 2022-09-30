1f06c287fd54ceb0182ab9eab92c801bdf29fb4c
kwonsohee <sohee3480@gmail.com>
2022-09-30 17:14:50 +0900

    02-1 코틀린 패키지

---
+++ app/src/main/java/com/example/doitkotlin/Person.kt
@@ -0,0 +1,7 @@
+package com.example.doitkotlin
+
+import com.example.doitkotlin.new.Person as User
+
+class Person {
+    val user1 = User()
+}
---
+++ app/src/main/java/com/example/doitkotlin/new/File.kt
@@ -0,0 +1,4 @@
+package com.example.doitkotlin.new
+
+class Person {
+}
